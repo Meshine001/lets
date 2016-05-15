128a41d2f3bcf00a288de1491ab651a524e541f4
Meshine <mingshuai001@hotmail.com>
2016-05-15 17:45:12 +0800

    add contacts

---
+++ application/controllers/contacts.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Meshine
+ * Date: 16/5/15
+ * Time: 下午2:48
+ */
+class Contacts extends MpController{
+
+
+    function doAdd(){
+
+    }
+
+    function doGet(){
+        
+    }
+}
